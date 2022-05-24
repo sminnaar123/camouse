ce86fbad3b138397306d8052e9436c20c6cef38d
Sydney Minnaar <sydneyminnaar2000@hotmail.com>
2022-05-24 23:40:50 +0200

    update

---
+++ nn-training/cnn.py
@@ -0,0 +1,11 @@
+import torch
+
+# Een neuraal netwerk voor het detecteren van de juiste kleur.
+# Welke kleur precies ligt aan de selectie van een afbeelding.
+# Als een afbeelding wordt gelabeled door een selectie van een groene kleur past het neurale netwerk zich hierop aan.
+
+# Creer neuraal netwerk [3, 3, 1]
+
+# Importeer afbeelding en label
+
+# Train het netwerk
