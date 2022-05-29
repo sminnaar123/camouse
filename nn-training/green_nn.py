933e7548cb128e765148379f2d4d0b52060c20cc
Sydney Minnaar <sydneyminnaar2000@hotmail.com>
2022-05-29 23:46:12 +0200

    Modified green_nn.py

--- nn-training/green_nn.py
+++ nn-training/green_nn.py
@@ -1,33 +1,28 @@
 import torch
 import numpy
-import math
 
 from PIL import Image
 
-debug = True
+# Configuration
+learning_rate = 0.00001
+epoch_amount = 4
+batch_size = 48
 
 # Select GPU for more performance (when cuda is available)
 if (torch.cuda.is_available()):
-
     device = torch.device("cuda")
-    if debug:
-        print("Cuda device has been selected.")
-
+    print("Cuda device has been selected.")
 else:
-
     device = torch.device("cpu")
-
+    print("No Cuda available, cpu has been selected.")
 
 # Define dataset
 class CustomDataset(torch.utils.data.Dataset):
     
     def __init__(self):
-
         super().__init__()
-
         image = Image.open("./images/green/1_green.jpg")
         self.x = torch.from_numpy(numpy.array(image.getdata(), dtype=numpy.float32))
-
         label = Image.open("./images/green/labels/1_green_labeled.jpg")
         pixels = list(label.getdata())
         self.y = []
@@ -39,79 +34,61 @@ class CustomDataset(torch.utils.data.Dataset):
         self.y = torch.from_numpy(numpy.array(self.y, dtype=numpy.float32))
 
     def __getitem__(self, index):
-
         return self.x[index], self.y[index]
 
     def __len__(self):
-
         return len(self.x)
 
 dataset = CustomDataset()
-dataloader = torch.utils.data.DataLoader(dataset, batch_size=480, shuffle=True, num_workers=12)
-
-print(dataset[0])
+dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=True, num_workers=12)
 
 
 # Create model
 class CustomNetwork(torch.nn.Module):
 
     def __init__(self):
-
         super().__init__()
-        self.input = torch.nn.Linear(3, 6)
-        self.output = torch.nn.Linear(6, 1)
+        self.first_hidden = torch.nn.Linear(3, 6)
+        self.second_hidden = torch.nn.Linear(6, 3)
+        self.output = torch.nn.Linear(3, 1)
         self.sigmoid = torch.nn.Sigmoid()
         
     def forward(self, x):
-
         # Pass the input tensor through each of our operations
-        x = self.input(x)
+        x = self.first_hidden(x)
+        x = self.second_hidden(x)
         x = self.output(x)
         x = self.sigmoid(x)
         return x
 
-model = CustomNetwork().cuda()
+model = CustomNetwork().to(device)
 
 # Train network
-learning_rate = 0.001
-epoch_amount = 1
-sample_amount = len(dataset)
-iteration_amount = math.ceil(sample_amount / 480)
-
+iteration_count = len(dataset) / batch_size
 loss = torch.nn.MSELoss()
 optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
 
 for epoch in range(epoch_amount):
-
     for i, (inputs, labels) in enumerate(dataloader):
 
         # Forward pass
-        prediction = model(inputs.cuda())
-        
+        prediction = model(inputs.to(device))
         # Determine loss
-        l = loss(labels.cuda(), prediction)
-
+        l = loss(labels.to(device), prediction)
         # Back propagation
         l.backward()
-
         # Update weight
         optimizer.step()
-
         # Reset gradients
         optimizer.zero_grad()
-
-        # Debug
-        if debug:
-            if (i+1) % 10 == 0:
-                print(f"loss = {l:.3f}")
-
-            with torch.no_grad():
-                for p, label in zip(prediction, labels):
-                    if label.item() > 1:
-                        print(f"predicted: {p.item():.3f}, target: {label.item():.3f}")
+        
+        if (i + 1) % batch_size == 0:
+            print(f"epoch = {epoch}/{epoch_amount}, step = {i+1}/{iteration_count}, loss = {l:.5f}")
 
 # Test model
 with torch.no_grad():
-    print(f"Prediction after training: f([0, 227, 137]) = {model(torch.tensor([0, 227, 137], dtype = torch.float32, device=device)).item():.3f}")
-    print(f"Prediction after training: f([0, 0, 0]) = {model(torch.tensor([0, 0, 0], dtype = torch.float32, device=device)).item():.3f}")
-    print(f"Prediction after training: f([0, 255, 0]) = {model(torch.tensor([0, 255, 0], dtype = torch.float32, device=device)).item():.3f}")
+    print(f"Prediction after training: f([0, 0, 0]) = {model(torch.tensor([0, 0, 0], dtype = torch.float32, device=device)).item():.5f}")
+    print(f"Prediction after training: f([255, 0, 0]) = {model(torch.tensor([0, 255, 0], dtype = torch.float32, device=device)).item():.5f}")
+    print(f"Prediction after training: f([0, 255, 0]) = {model(torch.tensor([0, 255, 0], dtype = torch.float32, device=device)).item():.5f}")
+    print(f"Prediction after training: f([0, 0, 255]) = {model(torch.tensor([0, 255, 0], dtype = torch.float32, device=device)).item():.5f}")
+    print(f"Prediction after training: f([0, 227, 137]) = {model(torch.tensor([0, 227, 137], dtype = torch.float32, device=device)).item():.5f}")
